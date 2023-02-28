f2246ee96ff5e1c295f4b3fc323e5ebbea3547b4
AmauxReav <pmaulana981@gmail.com>
2023-02-28 13:35:56 +0700

    fix

--- level-akses/main.go
+++ level-akses/main.go
@@ -1,9 +1,20 @@
 package main
+/*
+Import dengan prefix tanda titik
+- package library di-import menggunakan tanda titik. Dengan
+itu, pemanggilan struct Student tidak perlu dengan menuliskan nama package
+nya
+ jika tidak diberi titik maka penulisannya library.Student pada var s1
 
+ ============================
+ pemanfaatan alias ketika import package 
+ contoh seperti di fmt
+*/
 import (
-	"fmt"
-	"level-akses/library"
+	f "fmt"
+	. "level-akses/library"
 )
+
 
 // langkah pertama
 /* func main () {
@@ -14,12 +25,14 @@ import (
 //langkah ke 2
 
 func main() {
-	var s1 = library.Student{Name: "ujang", Age: 21, Addres: "Metro"}
-	fmt.Println(s1.Name)
-	fmt.Println(s1.Age)
+	var s1 = Student{Name: "ujang", Age: 21, Addres: "Metro"}
+	f.Println(s1.Name)
+	f.Println(s1.Age)
 }
 
 /*note : Dari contoh program di atas, bisa disimpulkan bahwa untuk menggunakan
 struct yang berada di package lain, selain nama struct-nya harus berbentuk
 exported, properti yang diakses juga harus exported juga.
 */
+
+// penjelasan yang  lengkap ada di halaman 121 
