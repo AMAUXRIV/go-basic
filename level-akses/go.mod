8b2c2a90d96f8bc16a032415ac757f1f2242cb4b
AmauxReav <pmaulana981@gmail.com>
2023-02-28 12:01:13 +0700

    basic

---
+++ level-akses/go.mod
@@ -0,0 +1,3 @@
+module level-akses
+
+go 1.19
---
+++ level-akses/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"level-akses/library"
+
+	
+)
+
+// langkah pertama
+/* func main () {
+	library.SayHello("amau")
+
+} */
+
+//langkah ke 2
+
+func main() {
+	var s1 = library.Student{Name: "ujang", Age: 21, Addres: "Metro"}
+	fmt.Println(s1.Name)
+	fmt.Println(s1.Age)
+}
+
+/*note : Dari contoh program di atas, bisa disimpulkan bahwa untuk menggunakan
+struct yang berada di package lain, selain nama struct-nya harus berbentuk
+exported, properti yang diakses juga harus exported juga.
+*/
