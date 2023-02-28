a317fd953fd0f6ed07e04b07e7f824f399839ca5
A M A U <97670164+AMAUXRIV@users.noreply.github.com>
2023-02-28 11:49:40 +0700

    GoBasic1

---
+++ level-akses/library/library.go
@@ -0,0 +1,20 @@
+package library
+
+//Langkah Pertama yang dicoba
+/* func SayHello(name string) {
+	fmt.Println("Haloo")
+	introduce(name)
+}
+
+func introduce(name string) {
+	fmt.Println("Nama Saya",name)
+}
+*/
+
+// langkah ke 2 : Penggunaan Hak akses exported / unexport terhadap struct dan property nya
+
+type Student struct {
+	Name   string
+	Age    int
+	Addres string
+}
