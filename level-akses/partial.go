8d0fda7cfd33279cda02fc0fe303008e79b4017d
A M A U <97670164+AMAUXRIV@users.noreply.github.com>
2023-03-01 17:59:42 +0700

    exported

--- level-akses/partial.go
+++ level-akses/partial.go
@@ -10,3 +10,19 @@ import f "fmt"
 func SayHello(s string) {
 	f.Println("Nama saya adalah",s)
 }
+
+func purchase(itemName string) int {
+	var itemPrices = map[string]int{
+		"soap":  4000,
+		"shampoo": 5000,
+		"brush":  2000,
+	}
+	
+	for key, value := range itemPrices {
+		if itemName == key {
+			return value
+		}
+	}
+	
+	return 0
+}
