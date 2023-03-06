079ffbe93b6d5e49addc8f983d51a1cdbfdaded6
AmauxReav <pmaulana981@gmail.com>
2023-03-06 12:09:23 +0700

    Nested NC

---
+++ structNested.go
@@ -0,0 +1,32 @@
+package main
+
+
+import "fmt"
+
+type Config struct {
+
+	Env string
+	Proxy Proxy
+}
+
+
+type Proxy struct {
+	Address string
+	Port string
+}
+
+
+func main () {
+
+		c := &Config{
+			Env : "DEBUG : TRUE",
+			Proxy: Proxy{
+				Address: "addr",
+				Port: "port",
+			},
+		}
+
+		fmt.Println ("Connect",c)
+		fmt.Println("alamat",c.Proxy.Address)
+
+}
